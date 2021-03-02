d0db8714c9543e3a75121346a5b0825e1b4db1d8
xinshaoguang <997888255@qq.com>
2021-03-02 15:09:26 +0800

    引入k-form-design

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import KFormDesign from 'k-form-design'
+import 'k-form-design/lib/k-form-design.css'
+Vue.use(KFormDesign)
 
 Vue.config.productionTip = false
 
